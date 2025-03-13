57360f34259a34a042f50812155083e398dcdd59
Ryosuke Hoda <h.ryosuke820@gmail.com>
2025-03-13 15:17:15 +0900

    fix: update button label to '送信' in the form component

--- components/Toolbar/Toolbar.tsx
+++ components/Toolbar/Toolbar.tsx
@@ -67,255 +67,243 @@ export const Toolbar = ({ editor }: ToolbarPropsType) => {
     }
   }, [editor]);
 
-  if (!editor) {
-    return null;
-  }
-
   if (!editor) return null;
 
   return (
-    <>
-      <div className="border-b border-gray-300 flex items-center py-2 px-5 gap-4 overflow-auto">
-        <button
-          type="button"
-          onClick={() =>
-            editor.chain().focus().toggleHeading({ level: 2 }).run()
-          }
-          className={`${
-            !editor.isActive("heading", { level: 2 }) ? "opacity-20" : ""
-          } cursor-pointer`}
-        >
-          <Heading2Icon />
-        </button>
-        <button
-          type="button"
-          onClick={() =>
-            editor.chain().focus().toggleHeading({ level: 3 }).run()
-          }
-          className={`${
-            !editor.isActive("heading", { level: 3 }) ? "opacity-20" : ""
-          } cursor-pointer`}
-        >
-          <Heading3Icon />
-        </button>
-        <button
-          type="button"
-          onClick={() =>
-            editor.chain().focus().toggleHeading({ level: 4 }).run()
-          }
-          className={`${
-            !editor.isActive("heading", { level: 4 }) ? "opacity-20" : ""
-          } cursor-pointer`}
-        >
-          <Heading4Icon />
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().toggleBulletList().run()}
-          className={`${
-            !editor.isActive("bulletList") ? "opacity-20" : ""
-          } cursor-pointer`}
-        >
-          <List />
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().toggleOrderedList().run()}
-          className={`${
-            !editor.isActive("orderedList") ? "opacity-20" : ""
-          } cursor-pointer`}
-        >
-          <ListOrderedIcon />
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().toggleBold().run()}
-          className={`${
-            !editor.isActive("bold") ? "opacity-20" : ""
-          } cursor-pointer`}
-        >
-          <BoldIcon />
-        </button>
-        <button
-          type="button"
-          onClick={setLink}
-          className={`${
-            editor.isActive("link") ? "is-active" : ""
-          } cursor-pointer`}
-        >
-          <Link2Icon />
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().unsetLink().run()}
-          disabled={!editor.isActive("link")}
-          className="cursor-pointer"
-        >
-          <Link2OffIcon />
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().toggleUnderline().run()}
-          className={`${
-            editor.isActive("underline") ? "is-active" : ""
-          } cursor-pointer`}
-        >
-          <UnderlineIcon />
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().toggleStrike().run()}
-          className={`${
-            !editor.isActive("strike") ? "opacity-20" : ""
-          } cursor-pointer`}
-        >
-          <StrikethroughIcon />
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().toggleCodeBlock().run()}
-          className={`${
-            !editor.isActive("codeBlock") ? "opacity-20" : ""
-          } cursor-pointer`}
-        >
-          <Code2Icon />
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().toggleBlockquote().run()}
-          className={`${
-            !editor.isActive("blockquote") ? "opacity-20" : ""
-          } cursor-pointer`}
-        >
-          <QuoteIcon />
-        </button>
+    <div className="border-b border-gray-300 flex items-center py-2 px-5 gap-4 overflow-auto">
+      <button
+        type="button"
+        onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+        className={`${
+          !editor.isActive("heading", { level: 2 }) ? "opacity-20" : ""
+        } cursor-pointer`}
+      >
+        <Heading2Icon />
+      </button>
+      <button
+        type="button"
+        onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+        className={`${
+          !editor.isActive("heading", { level: 3 }) ? "opacity-20" : ""
+        } cursor-pointer`}
+      >
+        <Heading3Icon />
+      </button>
+      <button
+        type="button"
+        onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
+        className={`${
+          !editor.isActive("heading", { level: 4 }) ? "opacity-20" : ""
+        } cursor-pointer`}
+      >
+        <Heading4Icon />
+      </button>
+      <button
+        type="button"
+        onClick={() => editor.chain().focus().toggleBulletList().run()}
+        className={`${
+          !editor.isActive("bulletList") ? "opacity-20" : ""
+        } cursor-pointer`}
+      >
+        <List />
+      </button>
+      <button
+        type="button"
+        onClick={() => editor.chain().focus().toggleOrderedList().run()}
+        className={`${
+          !editor.isActive("orderedList") ? "opacity-20" : ""
+        } cursor-pointer`}
+      >
+        <ListOrderedIcon />
+      </button>
+      <button
+        type="button"
+        onClick={() => editor.chain().focus().toggleBold().run()}
+        className={`${
+          !editor.isActive("bold") ? "opacity-20" : ""
+        } cursor-pointer`}
+      >
+        <BoldIcon />
+      </button>
+      <button
+        type="button"
+        onClick={setLink}
+        className={`${
+          editor.isActive("link") ? "is-active" : ""
+        } cursor-pointer`}
+      >
+        <Link2Icon />
+      </button>
+      <button
+        type="button"
+        onClick={() => editor.chain().focus().unsetLink().run()}
+        disabled={!editor.isActive("link")}
+        className="cursor-pointer"
+      >
+        <Link2OffIcon />
+      </button>
+      <button
+        type="button"
+        onClick={() => editor.chain().focus().toggleUnderline().run()}
+        className={`${
+          editor.isActive("underline") ? "is-active" : ""
+        } cursor-pointer`}
+      >
+        <UnderlineIcon />
+      </button>
+      <button
+        type="button"
+        onClick={() => editor.chain().focus().toggleStrike().run()}
+        className={`${
+          !editor.isActive("strike") ? "opacity-20" : ""
+        } cursor-pointer`}
+      >
+        <StrikethroughIcon />
+      </button>
+      <button
+        type="button"
+        onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+        className={`${
+          !editor.isActive("codeBlock") ? "opacity-20" : ""
+        } cursor-pointer`}
+      >
+        <Code2Icon />
+      </button>
+      <button
+        type="button"
+        onClick={() => editor.chain().focus().toggleBlockquote().run()}
+        className={`${
+          !editor.isActive("blockquote") ? "opacity-20" : ""
+        } cursor-pointer`}
+      >
+        <QuoteIcon />
+      </button>
 
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().setHorizontalRule().run()}
-          className="cursor-pointer"
-        >
-          <BetweenHorizontalStartIcon />
-        </button>
+      <button
+        type="button"
+        onClick={() => editor.chain().focus().setHorizontalRule().run()}
+        className="cursor-pointer"
+      >
+        <BetweenHorizontalStartIcon />
+      </button>
 
-        <button onClick={addImage} className="cursor-pointer">
-          <ImageIcon />
-        </button>
+      <button onClick={addImage} className="cursor-pointer">
+        <ImageIcon />
+      </button>
 
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().undo().run()}
-          disabled={!editor.can().undo()}
-          className="cursor-pointer"
+      <button
+        type="button"
+        onClick={() => editor.chain().focus().undo().run()}
+        disabled={!editor.can().undo()}
+        className="cursor-pointer"
+      >
+        <Undo2Icon />
+      </button>
+      <button
+        type="button"
+        onClick={() => editor.chain().focus().redo().run()}
+        disabled={!editor.can().redo()}
+        className="cursor-pointer"
+      >
+        <Redo2Icon />
+      </button>
+      {editor && (
+        <FloatingMenu
+          editor={editor}
+          tippyOptions={{ duration: 100 }}
+          className="md:block hidden"
         >
-          <Undo2Icon />
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().redo().run()}
-          disabled={!editor.can().redo()}
-          className="cursor-pointer"
-        >
-          <Redo2Icon />
-        </button>
-        {editor && (
-          <FloatingMenu
-            editor={editor}
-            tippyOptions={{ duration: 100 }}
-            className="md:block hidden"
-          >
-            <div data-testid="floating-menu" className="floating-menu">
-              <button
-                type="button"
-                onClick={() =>
-                  editor.chain().focus().toggleHeading({ level: 2 }).run()
-                }
-                className={`${
-                  editor.isActive("heading", { level: 2 }) ? "is-active" : ""
-                } cursor-pointer`}
-              >
-                <Heading2Icon />
-              </button>
-              <button
-                type="button"
-                onClick={() =>
-                  editor.chain().focus().toggleHeading({ level: 3 }).run()
-                }
-                className={`${
-                  editor.isActive("heading", { level: 3 }) ? "is-active" : ""
-                } cursor-pointer`}
-              >
-                <Heading3Icon />
-              </button>
-              <button
-                type="button"
-                onClick={() =>
-                  editor.chain().focus().toggleHeading({ level: 4 }).run()
-                }
-                className={`${
-                  editor.isActive("heading", { level: 4 }) ? "is-active" : ""
-                } cursor-pointer`}
-              >
-                <Heading4Icon />
-              </button>
-              <button
-                type="button"
-                onClick={() => editor.chain().focus().toggleBold().run()}
-                className={`${
-                  editor.isActive("bold") ? "is-active" : ""
-                } cursor-pointer`}
-              >
-                <BoldIcon />
-              </button>
-              <button
-                type="button"
-                onClick={() => editor.chain().focus().toggleBulletList().run()}
-                className={`${
-                  editor.isActive("bulletList") ? "is-active" : ""
-                } cursor-pointer`}
-              >
-                <List />
-              </button>
-              <button
-                type="button"
-                onClick={() => editor.chain().focus().toggleOrderedList().run()}
-                className={`${
-                  editor.isActive("orderedList") ? "is-active" : ""
-                } cursor-pointer`}
-              >
-                <ListOrderedIcon />
-              </button>
+          <div data-testid="floating-menu" className="floating-menu">
+            <button
+              type="button"
+              onClick={() =>
+                editor.chain().focus().toggleHeading({ level: 2 }).run()
+              }
+              className={`${
+                editor.isActive("heading", { level: 2 }) ? "is-active" : ""
+              } cursor-pointer`}
+            >
+              <Heading2Icon />
+            </button>
+            <button
+              type="button"
+              onClick={() =>
+                editor.chain().focus().toggleHeading({ level: 3 }).run()
+              }
+              className={`${
+                editor.isActive("heading", { level: 3 }) ? "is-active" : ""
+              } cursor-pointer`}
+            >
+              <Heading3Icon />
+            </button>
+            <button
+              type="button"
+              onClick={() =>
+                editor.chain().focus().toggleHeading({ level: 4 }).run()
+              }
+              className={`${
+                editor.isActive("heading", { level: 4 }) ? "is-active" : ""
+              } cursor-pointer`}
+            >
+              <Heading4Icon />
+            </button>
+            <button
+              type="button"
+              onClick={() => editor.chain().focus().toggleBold().run()}
+              className={`${
+                editor.isActive("bold") ? "is-active" : ""
+              } cursor-pointer`}
+            >
+              <BoldIcon />
+            </button>
+            <button
+              type="button"
+              onClick={() => editor.chain().focus().toggleBulletList().run()}
+              className={`${
+                editor.isActive("bulletList") ? "is-active" : ""
+              } cursor-pointer`}
+            >
+              <List />
+            </button>
+            <button
+              type="button"
+              onClick={() => editor.chain().focus().toggleOrderedList().run()}
+              className={`${
+                editor.isActive("orderedList") ? "is-active" : ""
+              } cursor-pointer`}
+            >
+              <ListOrderedIcon />
+            </button>
 
-              <button
-                type="button"
-                onClick={setLink}
-                className={`${
-                  editor.isActive("link") ? "is-active" : ""
-                } cursor-pointer`}
-              >
-                <Link2Icon />
-              </button>
-              <button
-                type="button"
-                onClick={() => editor.chain().focus().unsetLink().run()}
-                disabled={editor.isActive("link")}
-                className="cursor-pointer"
-              >
-                <Link2OffIcon />
-              </button>
-              <button
-                type="button"
-                onClick={() => editor.chain().focus().toggleUnderline().run()}
-                className={`${
-                  editor.isActive("underline") ? "is-active" : ""
-                } cursor-pointer`}
-              >
-                <UnderlineIcon />
-              </button>
-            </div>
-          </FloatingMenu>
-        )}
-      </div>
-    </>
+            <button
+              type="button"
+              onClick={setLink}
+              className={`${
+                editor.isActive("link") ? "is-active" : ""
+              } cursor-pointer`}
+            >
+              <Link2Icon />
+            </button>
+            <button
+              type="button"
+              onClick={() => editor.chain().focus().unsetLink().run()}
+              disabled={editor.isActive("link")}
+              className="cursor-pointer"
+            >
+              <Link2OffIcon />
+            </button>
+            <button
+              type="button"
+              onClick={() => editor.chain().focus().toggleUnderline().run()}
+              className={`${
+                editor.isActive("underline") ? "is-active" : ""
+              } cursor-pointer`}
+            >
+              <UnderlineIcon />
+            </button>
+          </div>
+        </FloatingMenu>
+      )}
+    </div>
   );
 };
